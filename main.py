0338fd8f5d8281d0596eb8a26e672b9214c1c228
Julien Larzul <julienlrzl@gmail.com>
2025-05-19 10:25:00 +0200

    interface main

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-if __name__ == "__main__":
-    
+# main.py
 
 from interface.console import jouer_partie_console
 from interface.gui import Puissance4GUI
 
+def menu_choix_interface():
+    print("\n--- Choix de l'interface ---")
+    print("1. Interface Console")
+    print("2. Interface Graphique (GUI)")
+    print("0. Quitter")
+    choix = input("Entrez votre choix : ")
+    return choix
+
 if __name__ == "__main__":
-    jouer_partie_console()
-    gui = Puissance4GUI()
-    gui.lancer()
+    choix = menu_choix_interface()
+
+    if choix == "1":
+        jouer_partie_console()
+    elif choix == "2":
+        gui = Puissance4GUI()
+        gui.lancer()
+    elif choix == "0":
+        print("À bientôt !")
+    else:
+        print("Choix invalide.")
 
